08f45ee5145ba6907712b59cdff509fce436b0e5
RavicharanN <ravicharan.vsp@gmail.com>
2017-11-20 03:18:55 +0530

    Add JvaApplication9 - Backend_without any UI

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author nvravicharan
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame form = new NewJFrame();
+        form.setVisible(true);
+    }
+    
+}
